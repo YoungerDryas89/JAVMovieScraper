cbd67d56190ec72becb879bffc03a3f7ed0f6fd2
Andrés O. Vázquez <sagittariusz548@gmail.com>
2023-12-16 17:09:53 -0700

    Finish conversion from Maven to Gradle 6

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module moviescraper.doctord {
+    requires javafx.swing;
+    requires javafx.controls;
+    requires commons.cli;
+    requires org.apache.commons.io;
+    requires java.desktop;
+    requires org.apache.commons.lang3;
+    requires org.jsoup;
+    requires imgscalr.lib;
+    requires json.io;
+    requires forms;
+    requires org.apache.httpcomponents.httpcore;
+    requires org.apache.httpcomponents.httpclient;
+    requires org.apache.commons.csv;
+    requires org.json;
+    requires java.logging;
+    requires org.apache.commons.codec;
+    requires xstream;
+    requires java.sql;
+    exports moviescraper.doctord;
+}
