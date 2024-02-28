da8093788758817c7fff2a569335e0ca13593c8c
Andrés O. Vázquez <sagittariusz548@gmail.com>
2024-02-28 13:45:55 -0700

    Update ActionJav parser profile and add new set of unit tests for it

--- src/main/java/moviescraper/doctord/controller/siteparsingprofile/specific/ActionJavParsingProfile.java
+++ src/main/java/moviescraper/doctord/controller/siteparsingprofile/specific/ActionJavParsingProfile.java
@@ -48,7 +48,7 @@ public class ActionJavParsingProfile extends SiteParsingProfile implements Speci
 	@Override
 	public Title scrapeTitle() {
 
-		Element titleElement = document.select("body table tbody tr td table tbody tr td div table tbody tr td table tbody tr td table tbody tr:contains(Video Title) td ~ td p").first();
+		Element titleElement = document.select("table.p-2 > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2) > span:nth-child(1)").first();
 		return new Title(titleElement.text());
 	}
 
@@ -75,13 +75,6 @@ public class ActionJavParsingProfile extends SiteParsingProfile implements Speci
 
 	@Override
 	public Rating scrapeRating() {
-		Element ratingElement = document.select("body table tbody tr td table tbody tr td div table tbody tr td table tbody tr td table tbody tr:contains(Rated) td ~ td p img").first();
-		if (ratingElement != null) {
-			String ratingImgUrl = ratingElement.attr("src");
-			String ratingIntegerIntegerPart = ratingImgUrl.substring(ratingImgUrl.length() - 7, ratingImgUrl.length() - 6);
-			String ratingDecimalIntegerPart = ratingImgUrl.substring(ratingImgUrl.length() - 5, ratingImgUrl.length() - 4);
-			return new Rating(5.0, ratingIntegerIntegerPart + "." + ratingDecimalIntegerPart);
-		}
 		return Rating.BLANK_RATING;
 	}
 
@@ -92,7 +85,7 @@ public class ActionJavParsingProfile extends SiteParsingProfile implements Speci
 
 	@Override
 	public ReleaseDate scrapeReleaseDate() {
-		Element releaseDateElement = document.select("body table tbody tr td table tbody tr td div table tbody tr td table tbody tr td table tbody tr:contains(Date Added) td ~ td p").first();
+		Element releaseDateElement = document.select("table.p-2 > tbody:nth-child(1) > tr:nth-child(7) > td:nth-child(2) > span:nth-child(1)").first();
 		if (releaseDateElement != null && releaseDateElement.text().length() > 4) {
 			String releaseDateText = releaseDateElement.text().trim();
 			if (!Character.isAlphabetic(releaseDateText.charAt(0))) //fix for weird white space trim() is not getting rid of
@@ -111,12 +104,6 @@ public class ActionJavParsingProfile extends SiteParsingProfile implements Speci
 
 	@Override
 	public Votes scrapeVotes() {
-		Element votesElement = document.select("body table tbody tr td table tbody tr td div table tbody tr td table tbody tr td table tbody tr:contains(Rated) td ~ td p font").first();
-		if (votesElement != null) {
-			String votes = votesElement.text();
-			votes = votes.substring(2, votes.indexOf('v') - 1);
-			return new Votes(votes);
-		}
 		return Votes.BLANK_VOTES;
 	}
 
@@ -127,13 +114,7 @@ public class ActionJavParsingProfile extends SiteParsingProfile implements Speci
 
 	@Override
 	public Plot scrapePlot() {
-
-		Element plotElement = document
-		        .select("body table tbody tr td table tbody tr td div table tbody tr td table tbody tr td table[width=372] tbody tr td table tbody tr td p[align=left] font[color=696981]").first();
-		if (plotElement != null)
-			return new Plot(plotElement.text().toString());
-		else
-			return Plot.BLANK_PLOT;
+		return Plot.BLANK_PLOT;
 	}
 
 	@Override
@@ -172,7 +153,7 @@ public class ActionJavParsingProfile extends SiteParsingProfile implements Speci
 				String runtimeText = movieElement.select("font").last().text();
 
 				//get whole text element
-				Integer runtimeAmt = new Integer(runtimeText.substring(1, runtimeText.indexOf('m') - 1));
+				Integer runtimeAmt = Integer.valueOf(runtimeText.substring(1, runtimeText.indexOf('m') - 1));
 				//narrow it down to just the numeric part since we want to ignore the other garbage in the string
 				runtimesByPart.put(fileNameNoExtension, runtimeAmt);
 			}
@@ -189,35 +170,21 @@ public class ActionJavParsingProfile extends SiteParsingProfile implements Speci
 		} else
 			return Runtime.BLANK_RUNTIME;
 	}
-
 	@Override
 	public Thumb[] scrapePosters() {
 		try {
-			Element posterImg = document.select("img[src*=/web_img/covers_hires_full/]").first();
-			//Thumb coverImageCrop = new Thumb(posterImg.attr("src"), 52.7, 0, 0,0);
-			Thumb coverImageCrop = new Thumb(posterImg.attr("src"), true);
-			//ActionJav has back and front cover in one jpg, so we need to crop to just get the movie poster
-			Thumb[] returnResult = new Thumb[1];
-			returnResult[0] = coverImageCrop;
-			return returnResult;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return new Thumb[0];
+			var posterElement = document.select("div.bg-white:nth-child(2) > div:nth-child(1) > center:nth-child(1) > a:nth-child(1) > img:nth-child(1)");
+			var thumb = new Thumb(posterElement.first().attr("src"));
+			return new Thumb[]{thumb};
+		}catch (MalformedURLException e){
+			System.err.println(e.getMessage());
 		}
+		return new Thumb[]{};
 	}
 
 	@Override
 	public Thumb[] scrapeFanart() {
-		try {
-			Element posterImg = document.select("img[src*=/web_img/covers_hires_full/]").first();
-			Thumb coverImageCrop = new Thumb(posterImg.attr("src"));
-			Thumb[] returnResult = new Thumb[1];
-			returnResult[0] = coverImageCrop;
-			return returnResult;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return new Thumb[0];
-		}
+		return new Thumb[]{};
 	}
 
 	@Override
@@ -227,49 +194,39 @@ public class ActionJavParsingProfile extends SiteParsingProfile implements Speci
 
 	@Override
 	public ID scrapeID() {
-		Element idElement = document.select("body table tbody tr td table tbody tr td div table tbody tr td table tbody tr td table tbody tr:contains(Publisher ID) td ~ td p").first();
+		Element idElement = document.select("table.p-2 > tbody:nth-child(1) > tr:nth-child(4) > td:nth-child(2) > span:nth-child(1)").first();
 
 		if (idElement != null) {
-			String idElementText = idElement.text();
-			int firstNumberIndex = StringUtils.indexOfAny(idElementText, "0123456789");
-			idElementText = idElementText.substring(0, firstNumberIndex) + "-" + idElementText.substring(firstNumberIndex);
-			return new ID(idElementText);
-		} else
-			return ID.BLANK_ID;
+			return new ID(idElement.text().split(" ")[0]);
+		}
+		return ID.BLANK_ID;
 	}
 
 	@Override
 	public ArrayList<Genre> scrapeGenres() {
-		Elements genreElements = document.select("body table tbody tr td table tbody tr td div table tbody tr td table tbody tr td table tbody tr:contains(Fetishes) td ~ td p");
-		ArrayList<Genre> genreList = new ArrayList<>(genreElements.size());
-		for (Element genreElement : genreElements) {
-			String genre = genreElement.select("a").first().attr("href");
-			genre = genre.substring(genre.indexOf('=') + 1);
-			genre = genre.replaceAll("_", " ");
-			genre = WordUtils.capitalizeFully(genre);
-			genreList.add(new Genre(genre));
-
+		Element genreElements = document.select("table.p-2 > tbody:nth-child(1) > tr:nth-child(9) > td:nth-child(2) > span:nth-child(1)").first();
+		var genres = new ArrayList<Genre>();
+        assert genreElements != null;
+        for(var genre : genreElements.text().split(" ")){
+			Genre gen = new Genre(genre);
+			genres.add(gen);
 		}
-		return genreList;
+		return genres;
 	}
 
 	@Override
 	public ArrayList<Actor> scrapeActors() {
-		Elements actorElements = document.select("body table tbody tr td table tbody tr td div table tbody tr td table tbody tr td table tbody tr:contains(Starring) td ~ td p");
+		Elements actorElements = document.select("table.p-2 > tbody:nth-child(1) > tr:nth-child(2) > td:nth-child(2) > span:nth-child(1) > a:nth-child(1)");
 		if (actorElements != null) {
 			ArrayList<Actor> actorList = new ArrayList<>(actorElements.size());
 			try {
 
 				for (Element actorElement : actorElements) {
-					String currentActorName = actorElement.select("font").first().text();
-					String currentActorDetailFileNameURL = actorElement.select("a").attr("href");
-					currentActorDetailFileNameURL = currentActorDetailFileNameURL.substring(currentActorDetailFileNameURL.indexOf('=') + 1);
-					currentActorDetailFileNameURL = "http://images2.tsunami-ent.com/web_img/av_idols_300/" + currentActorDetailFileNameURL + ".jpg";
-					Actor currentActor = new Actor(currentActorName, "", new Thumb(currentActorDetailFileNameURL));
+					Actor currentActor = new Actor(actorElement.text(), "", null);
 					actorList.add(currentActor);
 				}
 				return actorList;
-			} catch (IOException e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
@@ -284,7 +241,7 @@ public class ActionJavParsingProfile extends SiteParsingProfile implements Speci
 
 	@Override
 	public Studio scrapeStudio() {
-		Element studioElement = document.select("body table tbody tr td table tbody tr td div table tbody tr td table tbody tr td table tbody tr:contains(Publisher) td ~ td p").first();
+		Element studioElement = document.select("table.p-2 > tbody:nth-child(1) > tr:nth-child(3) > td:nth-child(2) > span:nth-child(1)").first();
 		return new Studio(studioElement.text());
 	}
 
@@ -296,7 +253,7 @@ public class ActionJavParsingProfile extends SiteParsingProfile implements Speci
 
 	@Override
 	public String createSearchStringFromId(String Id) {
-		return "http://www.actionjav.com/results_title.cfm?sortby=pub_idu&direction=ASC&searchterm=" + Id.replace("-", "");
+		return "https://www.actionjav.com/?view=search&item=" + Id;
 	}
 
 	@Override
@@ -304,50 +261,23 @@ public class ActionJavParsingProfile extends SiteParsingProfile implements Speci
 		if (searchString == null)
 			return new SearchResult[0];
 
-		LinkedList<SearchResult> searchItems = new LinkedList<>();
-		String searchId = searchString.replaceAll(".*searchterm=(\\D+)(\\d+)", "$1-$2").toUpperCase();
-		Document doc = Jsoup.connect(searchString).timeout(CONNECTION_TIMEOUT_VALUE).get();
-		Elements rows = doc.select("table table table tr:has(a[href^=title.cfm?iid=])");
+		var searchItems = new ArrayList<SearchResult>();
 
-		for (Element row : rows) {
-			String id = row.select("td:nth-child(2)").first().text().replaceAll("(\\D+)(\\d+)", "$1-$2").toUpperCase();
-			Element link = row.select("a[href^=title.cfm?iid=]").first();
-			Element actress = row.select("a[href^=model.cfm?actress_filename=]").first();
-			String title = "[" + id + "] " + link.text();
-			if (actress != null)
-				title = title + " - " + actress.ownText();
+		var searchDoc = Jsoup.connect(searchString).userAgent(getRandomUserAgent()).get();
+		var elems = searchDoc.select("div.card");
 
-			String url = "http://www.actionjav.com/" + link.attr("href");
-			SearchResult result = new SearchResult(url, title);
-
-			if (id.equals(searchId))
-				searchItems.addFirst(result);
-			else
-				searchItems.addLast(result);
+		for(var elem : elems){
+			var url_element = elem.getElementsByTag("a").first();
+			var title = elem.getElementsByClass("movie-list-title").first().text();
+			SearchResult result = new SearchResult(url_element.attr("href"), title, new Thumb(url_element.childNode(1).attr("src")));
+			searchItems.add(result);
 		}
-
 		return searchItems.toArray(new SearchResult[searchItems.size()]);
 	}
 
 	@Override
 	public Thumb[] scrapeExtraFanart() {
-		ArrayList<Thumb> imageList = new ArrayList<>();
-
-		Element script = document.select("head > script:nth-of-type(2)").first();
-		if (script != null) {
-			String data = script.data();
-			Pattern pattern = Pattern.compile("\"(http://images2.tsunami-ent.com/web_img/.*\\.jpg)\"");
-			Matcher matcher = pattern.matcher(data);
-			while (matcher.find()) {
-				try {
-					imageList.add(new Thumb(matcher.group(1)));
-				} catch (MalformedURLException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		return imageList.toArray(new Thumb[imageList.size()]);
+		return new Thumb[]{};
 	}
 
 	@Override
