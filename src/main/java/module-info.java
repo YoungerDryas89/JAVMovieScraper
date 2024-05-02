19888e86f00b0b8ccd206b6bbe253a0dc7f1e812
Andrés O. Vázquez <sagittariusz548@gmail.com>
2024-05-02 03:52:11 -0600

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -17,6 +17,11 @@ module moviescraper.doctord {
     requires org.apache.commons.codec;
     requires xstream;
     requires java.sql;
+    requires org.seleniumhq.selenium.api;
+    requires org.seleniumhq.selenium.chrome_driver;
+    requires dev.failsafe.core;
+    requires org.seleniumhq.selenium.firefox_driver;
+    requires fastjson;
     exports moviescraper.doctord;
     exports moviescraper.doctord.controller.amalgamation;
     exports moviescraper.doctord.controller.siteparsingprofile;
