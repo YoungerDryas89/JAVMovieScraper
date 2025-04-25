e49730e67da814b7b0ce23201365ba5f4ef73894
Andrés O. Vázquez <sagittariusz548@gmail.com>
2025-04-25 13:16:12 -0600

    Expand Language enums and add `getSupportedLanguages` `SpecificProfile` interface

--- src/main/java/moviescraper/doctord/controller/languagetranslation/Language.java
+++ src/main/java/moviescraper/doctord/controller/languagetranslation/Language.java
@@ -1,5 +1,20 @@
 package moviescraper.doctord.controller.languagetranslation;
 
 public enum Language {
-	ENGLISH, JAPANESE, CHINESE, TAIWANESE
+	ENGLISH,
+	JAPANESE,
+	CHINESE,
+	TAIWANESE,
+	KOREAN,
+	SPANISH,
+	CHINESE_SIMPLIFIED,
+	MALAYSIAN,
+	THAI,
+	VIETNAMESE,
+	INDONESIAN,
+	FILIPINO,
+	PORTUGESE,
+	GERMAN,
+	FRENCH,
+	HINDI
 }
--- src/main/java/moviescraper/doctord/controller/siteparsingprofile/specific/SpecificProfile.java
+++ src/main/java/moviescraper/doctord/controller/siteparsingprofile/specific/SpecificProfile.java
@@ -1,8 +1,13 @@
 package moviescraper.doctord.controller.siteparsingprofile.specific;
 
+import moviescraper.doctord.controller.languagetranslation.Language;
+
+import java.util.List;
+
 public interface SpecificProfile {
 
 	//Name to show in spinner
 	String getParserName();
+	Language[] getSupportedLanguages();
 
 }
