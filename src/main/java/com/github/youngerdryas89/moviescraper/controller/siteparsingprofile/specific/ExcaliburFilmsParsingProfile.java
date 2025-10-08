b551ff4398f222a49cddbfcc667c521f8461cb06
Andrés O. Vázquez <sagittariusz548@gmail.com>
2025-10-08 15:33:39 -0600

    Filter out any noise so we can get the title directly

--- src/main/java/com/github/youngerdryas89/moviescraper/controller/siteparsingprofile/specific/ExcaliburFilmsParsingProfile.java
+++ src/main/java/com/github/youngerdryas89/moviescraper/controller/siteparsingprofile/specific/ExcaliburFilmsParsingProfile.java
@@ -377,7 +377,7 @@ public class ExcaliburFilmsParsingProfile extends SiteParsingProfile implements 
 			fileBaseName = file.getName();
 
         fileBaseName = fileBaseName.replaceAll("(19\\d\\d|20\\d\\d)", "");
-        fileBaseName = fileBaseName.replaceAll("[()]", "");
+        fileBaseName = fileBaseName.replaceAll("[()\\[\\]\\.\\-_#]", "");
         fileBaseName = fileBaseName.replaceAll("(1080p?|720p?|480p?|360p?|240p?|144p?|4k|4K|2160p?)", "");
 		String[] splitBySpace = fileBaseName.split(" ");
 		if (splitBySpace.length > 1) {
@@ -454,3 +454,4 @@ public class ExcaliburFilmsParsingProfile extends SiteParsingProfile implements 
 	}
 
 }
+
