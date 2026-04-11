9fdc11407b38533eb62c31a59c8fcec5695ca40c
Andrés Vázquez <sagittariusz548@gmail.com>
2026-04-11 07:35:17 -0600

    Create ScrapeMetadata for managing contextual info about the scrape operation instead of passing individual parameters

---
+++ src/main/kotlin/com/github/youngerdryas89/moviescraper/model/ScrapeMetadata.kt
@@ -0,0 +1,23 @@
+package com.github.youngerdryas89.moviescraper.model
+
+import arrow.core.Either
+import com.github.youngerdryas89.moviescraper.controller.siteparsingprofile.SiteParsingProfile
+import com.github.youngerdryas89.moviescraper.controller.siteparsingprofile.SpecificProfileInfo
+import com.github.youngerdryas89.moviescraper.controller.siteparsingprofile.SpecificProfileSettings
+import java.io.File
+import java.nio.file.Path
+
+typealias SortedMovieFiles = List<File>
+typealias MovieFile = File
+typealias CleansedTitle = String
+
+data class ScrapeMetadata (
+    val directory: Path,
+    val parentDirectory: Path,
+    val movie: Either<MovieFile, SortedMovieFiles>,
+    val siteProfile: SiteParsingProfile,
+    val siteProfileInfo: SpecificProfileInfo,
+    val siteProfileSettings: SpecificProfileSettings,
+    val cleansedFilename: CleansedTitle? = null,
+    val finishedMovie: MovieData? = null
+)
