75a24ced49893e208f44a7779d3ab0830ec69e0f
Andrés Vázquez <sagittariusz548@gmail.com>
2026-04-12 17:18:33 -0600

    Add context as input to each function and make them optional for now

--- src/main/kotlin/com/github/youngerdryas89/moviescraper/controller/siteparsingprofile/scrapers/ScraperProfile.kt
+++ src/main/kotlin/com/github/youngerdryas89/moviescraper/controller/siteparsingprofile/scrapers/ScraperProfile.kt
@@ -1,10 +1,12 @@
 package com.github.youngerdryas89.moviescraper.controller.siteparsingprofile.scrapers
 
+import com.github.youngerdryas89.moviescraper.model.ScrapeMetadata
 import com.github.youngerdryas89.moviescraper.model.dataitem.Actor
 import com.github.youngerdryas89.moviescraper.model.dataitem.Actresses
 import com.github.youngerdryas89.moviescraper.model.dataitem.Director
 import com.github.youngerdryas89.moviescraper.model.dataitem.Directors
 import com.github.youngerdryas89.moviescraper.model.dataitem.Genre
+import com.github.youngerdryas89.moviescraper.model.dataitem.ID
 import com.github.youngerdryas89.moviescraper.model.dataitem.Identity
 import com.github.youngerdryas89.moviescraper.model.dataitem.MPAARating
 import com.github.youngerdryas89.moviescraper.model.dataitem.OriginalTitle
@@ -19,34 +21,35 @@ import com.github.youngerdryas89.moviescraper.model.dataitem.Studio
 import com.github.youngerdryas89.moviescraper.model.dataitem.Tag
 import com.github.youngerdryas89.moviescraper.model.dataitem.Tagline
 import com.github.youngerdryas89.moviescraper.model.dataitem.Title
+import com.github.youngerdryas89.moviescraper.model.dataitem.Titling
 import com.github.youngerdryas89.moviescraper.model.dataitem.Top250
 import com.github.youngerdryas89.moviescraper.model.dataitem.Trailer
 import com.github.youngerdryas89.moviescraper.model.dataitem.Votes
 import com.github.youngerdryas89.moviescraper.model.dataitem.Year
 
 data class ScraperProfile(
-    val scrapeTitle: () -> Title,
-    val scrapeOriginalTitle: () -> OriginalTitle,
-    val scrapeSortTitle: () -> SortTitle,
-    val scrapeSet: () -> Series,
-    val scrapeRating: () -> Rating,
-    val scrapeReleaseDate: () -> ReleaseDate,
-    val scrapeYear: () -> Year,
-    val scrapeTop250: () -> Top250,
-    val scrapeVotes: () -> Votes,
-    val scrapeOutline: () -> Outline,
-    val scrapePlot: () -> Plot,
-    val scrapeTagline: () -> Tagline,
-    val scrapeRuntime: () -> Runtime,
-    val scrapePosters: (cropPosters: Boolean) -> List<String>,
-    val scrapeFanart: () -> List<String>,
-    val scrapeExtraFanart: () -> List<String>,
-    val scrapeMPAA: () -> MPAARating,
-    val scrapeID: () -> Identity.Id,
-    val scrapeGenres: () -> List<Genre>,
-    val scrapeActors: () -> Actresses,
-    val scrapeDirectors: () -> Directors,
-    val scrapeStudio: () -> Studio,
-    val scrapeTrailer: () -> Trailer,
-    val scrapeTags: () -> List<Tag>
+    val scrapeTitle: (context: ScrapeMetadata) -> Titling.Title?,
+    val scrapeOriginalTitle: (context: ScrapeMetadata) -> Titling.OriginalTitle?,
+    val scrapeSortTitle: (context: ScrapeMetadata) -> Titling.SortTitle?,
+    val scrapeSet: (context: ScrapeMetadata) -> Series?,
+    val scrapeRating: (context: ScrapeMetadata) -> Rating?,
+    val scrapeReleaseDate: (context: ScrapeMetadata) -> ReleaseDate?,
+    val scrapeYear: (context: ScrapeMetadata) -> Year?,
+    val scrapeTop250: (context: ScrapeMetadata) -> Top250?,
+    val scrapeVotes: (context: ScrapeMetadata) -> Votes?,
+    val scrapeOutline: (context: ScrapeMetadata) -> Outline?,
+    val scrapePlot: (context: ScrapeMetadata) -> Plot?,
+    val scrapeTagline: (context: ScrapeMetadata) -> Tagline?,
+    val scrapeRuntime: (context: ScrapeMetadata) -> Runtime?,
+    val scrapePosters: (context: ScrapeMetadata) -> List<String>,
+    val scrapeFanart: (context: ScrapeMetadata) -> List<String>,
+    val scrapeExtraFanart: (context: ScrapeMetadata) -> List<String>,
+    val scrapeMPAA: (context: ScrapeMetadata) -> MPAARating,
+    val scrapeID: (context: ScrapeMetadata) -> ID?,
+    val scrapeGenres: (context: ScrapeMetadata) -> List<Genre>,
+    val scrapeActors: (context: ScrapeMetadata) -> Actresses,
+    val scrapeDirectors: (context: ScrapeMetadata) -> Directors,
+    val scrapeStudio: (context: ScrapeMetadata) -> Studio?,
+    val scrapeTrailer: (context: ScrapeMetadata) -> Trailer?,
+    val scrapeTags: (context: ScrapeMetadata) -> List<Tag>
 )
