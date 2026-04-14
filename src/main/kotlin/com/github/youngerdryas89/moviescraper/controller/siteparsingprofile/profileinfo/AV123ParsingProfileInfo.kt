c09504205b8fbe60c3eb1d63b2db41c9379fc1c8
Andrés Vázquez <sagittariusz548@gmail.com>
2026-04-14 13:39:18 -0600

    Unfinished: AV123ParsingProfileInfo

--- src/main/kotlin/com/github/youngerdryas89/moviescraper/controller/siteparsingprofile/profileinfo/AV123ParsingProfileInfo.kt
+++ src/main/kotlin/com/github/youngerdryas89/moviescraper/controller/siteparsingprofile/profileinfo/AV123ParsingProfileInfo.kt
@@ -1,8 +1,42 @@
 package com.github.youngerdryas89.moviescraper.controller.siteparsingprofile.profileinfo
 
+import arrow.core.Either
+import arrow.core.Either.Companion.catch
+import arrow.core.flatMap
 import arrow.core.nonEmptyListOf
+import arrow.core.raise.catch
+import arrow.core.raise.context.bind
+import arrow.core.raise.either
+import arrow.core.raise.ensure
+import arrow.core.right
+import com.github.youngerdryas89.moviescraper.controller.getUnstackedMovieName
 import com.github.youngerdryas89.moviescraper.controller.languagetranslation.Language
+import com.github.youngerdryas89.moviescraper.controller.siteparsingprofile.DerivedURL
+import com.github.youngerdryas89.moviescraper.controller.siteparsingprofile.SearchStringInput
 import com.github.youngerdryas89.moviescraper.controller.siteparsingprofile.SpecificProfileInfo
+import com.github.youngerdryas89.moviescraper.controller.siteparsingprofile.scrapers.DomainError
+import com.github.youngerdryas89.moviescraper.controller.siteparsingprofile.specific.AV123ParsingProfile
+import com.github.youngerdryas89.moviescraper.model.ScrapeMetadata
+import com.github.youngerdryas89.moviescraper.scraper.UserAgent
+import org.jsoup.Jsoup
+
+
+fun createURL(input: SearchStringInput, language: Language) : DerivedURL =
+    DerivedURL.DirectURL(AV123ParsingProfileInfo.domainName + AV123ParsingProfileInfo.domainNames[language] + input.attribute)
+
+fun extractTableData(context: ScrapeMetadata, attribute: String) : Either<DomainError, Map<String, String>> =
+    either {
+        val message = catch{ context.document?.expectFirst(".message")?.text()!! }
+        ensure(message.isLeft()) { DomainError.ScrapingError("The selected title could not be found.")}
+        val table = catch{ context.document?.expectFirst(".content .detail-item")?.children()!!}
+        ensure(table.) { DomainError.Nothing }
+        val pairs = table.
+
+        mapof
+
+    }
+
+
 
 val AV123ParsingProfileInfo = SpecificProfileInfo(
     name = "123AV",
@@ -35,5 +69,10 @@ val AV123ParsingProfileInfo = SpecificProfileInfo(
         Language.HINDI to "/hi/"
     ),
     profileIconFilename = "123AV.png",
-    domainName = "123av.com"
+    domainName = "123av.com",
+    cleanseFilename = { fn  -> fn.getUnstackedMovieName() },
+    fetchSearchResults = throw NotImplementedError(),
+    fetchDirectPage = { url -> Jsoup.connect(url).userAgent(UserAgent.getRandomUserAgent()).response() },
+    createURLFromInput = ::createURL,
+    scraperProfile = TODO()
 )
