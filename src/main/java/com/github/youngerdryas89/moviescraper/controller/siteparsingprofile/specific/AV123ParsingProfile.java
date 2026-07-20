d73ab085b7bd8510b8a5d455e436e22db33b826d
Andrés O. Vázquez <sagittariusz548@gmail.com>
2026-07-20 13:47:37 -0600

    Update 123AV scraper to reflect the new website

--- src/main/java/com/github/youngerdryas89/moviescraper/controller/siteparsingprofile/specific/AV123ParsingProfile.java
+++ src/main/java/com/github/youngerdryas89/moviescraper/controller/siteparsingprofile/specific/AV123ParsingProfile.java
@@ -10,33 +10,35 @@ import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import javax.annotation.Nonnull;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.regex.Pattern;
 
 public class AV123ParsingProfile extends SiteParsingProfile implements SpecificProfile {
 
-    final String titlePath = "html body div#app div#body div#page-video.container div.row div.col div.d-flex.justify-content-between.align-items-start div.mr-3 h1";
-    final String posterPath = "html body div#app div#body div#page-video.container div.row div.col div#player";
-    final String movieDetailsPath = ".content .detail-item";
-    final String plotPath = "html body div#app div#body div#page-video.container div.row div.col div#details div.content div.description p";
+    final String titlePath = "h1.watch__title";
+    final String posterPath = "div.player";
+    final Pattern posterRegexExtract = Pattern.compile("background-image\\s*:\\s*url\\(['\"]?(?<url>[^'\"]+?)['\"]?\\)", Pattern.CASE_INSENSITIVE);
+    final String movieDetailsPath = ".watch__info-row";
+    final String plotPath = "p.watch__desc-text";
     Map<String, Element> movie_data = new HashMap<>();
     String id, url;
     Document japaneseDocument;
 
     @Override
     public void prepareData(){
-        Element message = document.select(".message").first();
-        if(message != null){
-            System.err.println(message.text());
+        Element err = document.select("div.errpage__code").first();
+        if(err != null && err.text().equals("404")){
+            System.err.println("Error: 404, No movie found!");
+
         } else {
-            Element table = document.select(movieDetailsPath).first();
-            if (table != null) {
-                for (Element data : table.children()) {
-                    movie_data.put(data.firstElementChild().text(), data.lastElementChild());
-                }
+            Elements tableElems = document.select(movieDetailsPath);
+            for (Element data : tableElems) {
+                movie_data.put(data.firstElementChild().text(), data.lastElementChild());
             }
         }
     }
@@ -66,7 +68,7 @@ public class AV123ParsingProfile extends SiteParsingProfile implements SpecificP
                 if(continueButton != null && continueButton.text().equals("Click here to continue")) {
                     String redirectUrl = continueButton.attr("href");
 
-                    System.out.println("AV123: Redirecting to: " + redirectUrl);
+                    System.out.println("123AV: Redirecting to: " + redirectUrl);
                     this.url = redirectUrl;
                     return Jsoup.connect(redirectUrl).userAgent(UserAgent.getRandomUserAgent()).followRedirects(true).ignoreHttpErrors(true).timeout(CONNECTION_TIMEOUT_VALUE).execute();
                 }
@@ -104,8 +106,8 @@ public class AV123ParsingProfile extends SiteParsingProfile implements SpecificP
     @Nonnull
     @Override
     public Set scrapeSet() {
-        if(movie_data.containsKey("Series:")){
-            return new Set(movie_data.get("Series:").text());
+        if(movie_data.containsKey("Series")){
+            return new Set(movie_data.get("Series").text());
         }
         return Set.BLANK_SET;
     }
@@ -119,8 +121,8 @@ public class AV123ParsingProfile extends SiteParsingProfile implements SpecificP
     @Nonnull
     @Override
     public ReleaseDate scrapeReleaseDate() {
-        if(movie_data.containsKey("Release date:")){
-            return new ReleaseDate(movie_data.get("Release date:").text());
+        if(movie_data.containsKey("Release date")){
+            return new ReleaseDate(movie_data.get("Release date").text());
         }
         return ReleaseDate.BLANK_RELEASEDATE;
     }
@@ -168,9 +170,9 @@ public class AV123ParsingProfile extends SiteParsingProfile implements SpecificP
     @Nonnull
     @Override
     public Runtime scrapeRuntime() {
-        if(movie_data.containsKey("Runtime:")){
+        if(movie_data.containsKey("Duration")){
             try {
-                Element durationElement = movie_data.get("Runtime:");
+                Element durationElement = movie_data.get("Duration");
                 if (durationElement != null) {
                     String[] durationSplitByTimeUnit = durationElement.text().split(":");
                     if (durationSplitByTimeUnit.length != 3) {
@@ -194,9 +196,13 @@ public class AV123ParsingProfile extends SiteParsingProfile implements SpecificP
     public Thumb[] scrapePosters(boolean cropPosters) {
         List<Thumb> posters = new ArrayList<>();
         try {
-            Element poster = document.select(posterPath).first();
-            if (poster != null) {
-                posters.add(new Thumb(poster.attr("data-poster"), cropPosters));
+            Element divElem = document.select(posterPath).first();
+            if (divElem != null) {
+                String styleTxt = divElem.attr("style");
+                var matched = posterRegexExtract.matcher(styleTxt);
+                if(matched.matches()) {
+                    posters.add(new Thumb(matched.group("url"), cropPosters));
+                }
             }
         }catch (IOException e){
             System.err.println(e.getMessage());
@@ -206,16 +212,7 @@ public class AV123ParsingProfile extends SiteParsingProfile implements SpecificP
 
     @Override
     public Thumb[] scrapeFanart() {
-        List<Thumb> posters = new ArrayList<>();
-        try {
-            Element poster = document.select(posterPath).first();
-            if (poster != null) {
-                posters.add(new Thumb(poster.attr("data-poster")));
-            }
-        }catch (IOException e){
-            System.err.println(e.getMessage());
-        }
-        return posters.toArray(new Thumb[0]);
+        return new Thumb[0];
     }
 
     @Override
@@ -232,8 +229,8 @@ public class AV123ParsingProfile extends SiteParsingProfile implements SpecificP
     @Nonnull
     @Override
     public ID scrapeID() {
-        if(movie_data.containsKey("Code:")){
-            return new ID(movie_data.get("Code:").text());
+        if(movie_data.containsKey("Code")){
+            return new ID(movie_data.get("Code").text());
         }
         return ID.BLANK_ID;
     }
@@ -242,8 +239,8 @@ public class AV123ParsingProfile extends SiteParsingProfile implements SpecificP
     @Override
     public ArrayList<Genre> scrapeGenres() {
         ArrayList<Genre> genres = new ArrayList<>();
-        if(movie_data.containsKey("Genres:")){
-            for(Element genre : movie_data.get("Genres:").children()){
+        if(movie_data.containsKey("Genres")){
+            for(Element genre : movie_data.get("Genres").children()){
                 genres.add(new Genre(genre.text()));
             }
         }
@@ -254,8 +251,8 @@ public class AV123ParsingProfile extends SiteParsingProfile implements SpecificP
     @Override
     public ArrayList<Actor> scrapeActors() {
         ArrayList<Actor> actresses = new ArrayList<>();
-        if(movie_data.containsKey("Actresses:")){
-            for(Element actress : movie_data.get("Actresses:").children()){
+        if(movie_data.containsKey("Cast")){
+            for(Element actress : movie_data.get("Cast").children()){
                actresses.add(new Actor(actress.text(), null, null));
             }
         }
@@ -271,11 +268,11 @@ public class AV123ParsingProfile extends SiteParsingProfile implements SpecificP
     @Nonnull
     @Override
     public Studio scrapeStudio() {
-        if(movie_data.containsKey("Maker:")){
-            return new Studio(movie_data.get("Maker:").text());
+        if(movie_data.containsKey("Maker")){
+            return new Studio(movie_data.get("Maker").text());
         } else {
-            if (movie_data.containsKey("Label:")) {
-                return new Studio(movie_data.get("Label:").text());
+            if (movie_data.containsKey("Label")) {
+                return new Studio(movie_data.get("Label").text());
             }
         }
         return Studio.BLANK_STUDIO;
@@ -314,8 +311,8 @@ public class AV123ParsingProfile extends SiteParsingProfile implements SpecificP
     @Override
     public ArrayList<Tag> scrapeTags(){
         ArrayList<Tag> tags = new ArrayList<>();
-        if(movie_data.containsKey("Tags:")){
-            for(var tag : movie_data.get("Tags:").children()){
+        if(movie_data.containsKey("Tags")){
+            for(var tag : movie_data.get("Tags").children()){
                 tags.add(new Tag(tag.text()));
             }
         }
